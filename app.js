4bf57daaccffd32080d09d2f3bdbb805f54ff289
Eric Hernández <erhh.015@gmail.com>
2023-12-06 12:06:39 -0600

    ToDo List working

---
+++ app.js
@@ -0,0 +1,53 @@
+const container = document.querySelector( ".container" );
+const listContainer = container.querySelector( "#list-container" );
+const inputBox = container.querySelector( "#input-box" );
+const addButton = container.querySelector( "button" );
+
+function addTask() {
+    
+    if ( inputBox.value === '' ) {
+        alert("Enter Some Data")
+    }
+    else {
+
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild( li );
+
+        let span = document.createElement( "span" );
+        span.innerHTML = "\u00d7";
+        li.appendChild( span ); 
+    }
+
+    inputBox.value = '';
+    saveTask();
+
+}
+
+listContainer.addEventListener( "click" , function(e) {
+
+    if( e.target.tagName === "LI" ) {
+       
+        e.target.classList.toggle( "checked" );
+
+    }
+    else if ( e.target.tagName === 'SPAN') {
+
+        e.target.parentElement.remove();
+        
+    }
+    saveTask();
+});
+
+function saveTask() {
+
+    localStorage.setItem( "data" , listContainer.innerHTML );
+}
+
+function showTask() {
+    
+    listContainer.innerHTML = localStorage.getItem( "data" );
+}
+
+addButton.addEventListener( 'click' , addTask );
+showTask();
